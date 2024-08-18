3206dfa69b3740ed3fcbad1e136379cfe3cdfe13
danilyef <daniil.yefimov92@gmail.com>
2024-08-18 17:34:29 +0200

    update task1

--- homework_2/task1/web.sh
+++ homework_2/task1/web.sh
@@ -5,13 +5,14 @@ IMAGE_NAME="python-server"
 TAG="latest"
 DOCKER_USER="yf19001"
 REPO_NAME="${DOCKER_USER}/${IMAGE_NAME}:${TAG}"
+CONTAINER_NAME="web-server"
 
 # Build image
 echo "Building Docker image..."
-docker build -t ${REPO_NAME} .
+docker build -t ${REPO_NAME} -f Dockerfile.web .
 
 
-if [$? -ne 0]; then
+if [ $? -ne 0 ]; then
     echo "Docker build failed. Exiting."
     exit 1
 fi 
@@ -22,10 +23,14 @@ echo "Logging in to Docker Hub..."
 docker login -u yf19001
 
 
-if [$? -ne 0]; then
+if [ $? -ne 0 ]; then
     echo "Docker login failed. Exiting"
     exit 1
 fi
+
+# Run Web Server:
+docker run -it --rm -p 80:80 --name "${CONTAINER_NAME}" "${REPO_NAME}"
+curl http://127.0.0.1:80
 
 # Push docker image
 docker push ${REPO_NAME}
