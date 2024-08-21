cf442e8121802d5cdc968b621416a7aa456c9367
danilyef <daniil.yefimov92@gmail.com>
2024-08-21 22:45:10 +0200

    PR5

--- homework_3/pr5/main.py
+++ homework_3/pr5/main.py
@@ -18,14 +18,13 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 
 
-
 # Prepare a sample dataset and model for benchmarking
 def create_model_and_data():
     # Create a synthetic regression dataset with 100 features
-    X, y = make_regression(n_samples=5000000, n_features=100, noise=0.1, random_state=42)
+    X, y = make_regression(n_samples=200000, n_features=100, noise=0.1, random_state=42)
     
-    # Split into training and testing setsx
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+    # Split into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.99, random_state=42)
     
     # Train a simple Linear Regression model
     model = LinearRegression()
@@ -34,44 +33,44 @@ def create_model_and_data():
     return model, X_test
 
 # Single inference task using the sklearn model
-def inference_task(model, data):
+def inference_task(args):
+    model, data = args
+    time.sleep(0.005)
     # Simulate model inference (predicting the data)
     return model.predict(data)
 
 
-
-
-def single_process_inference(model,batches):
+def single_process_inference(model, batches):
     start_time = time.time()
 
     for batch in batches:
-        inference_task(model,batch)
-
+        inference_task((model, batch))
 
     elapsed_time = time.time() - start_time
     return elapsed_time
 
 
-
-
-def multiple_process_inference(model,batches,num_processes = 4):
-    
+def multiple_process_inference(model, batches, num_processes=16):
     start_time = time.time()
 
     with mp.Pool(processes=num_processes) as pool:
-        pool.starmap(inference_task,[(model, batch) for batch in batches])
-    
+        pool.map(inference_task, [(model, batch) for batch in batches])
 
     elapsed_time = time.time() - start_time
     return elapsed_time
 
-if __name__ == '__main__': 
-    model, X_test  = create_model_and_data()
 
-    batch_size = 128
+if __name__ == '__main__':
+    model, X_test = create_model_and_data()
+
+    batch_size = 100
     num_batches = len(X_test) // batch_size + (1 if len(X_test) % batch_size != 0 else 0)
 
     data_batches = np.array_split(X_test, num_batches)
 
-    print(single_process_inference(model,data_batches))
-    print(multiple_process_inference(model,data_batches))
+    print(f"Single process inference time: {single_process_inference(model, data_batches)} seconds")
+    print(f"Multiple process inference time: {multiple_process_inference(model, data_batches)} seconds")
+
+
+# Multiprocessing is faster, when batch_size is small
+# Multiprocessing is slower, when batch_bisze is bigger. 
