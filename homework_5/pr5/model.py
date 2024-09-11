c3bd995dfe1bfec7a7a1bfec5690b4369072bac7
danilyef <daniil.yefimov92@gmail.com>
2024-09-11 22:08:02 +0200

    changes

--- homework_5/pr5/model.py
+++ homework_5/pr5/model.py
@@ -6,39 +6,43 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 
 
-params = {
-    'features': 256,
-    'lr': 0.001,
-    'momentum': 0,
-}
-
-optimized_params = nni.get_next_parameter()
-params.update(optimized_params)
-print(params)
-
-
-training_data = datasets.FashionMNIST(root="data", train=True, download=True, transform=ToTensor())
-test_data = datasets.FashionMNIST(root="data", train=False, download=True, transform=ToTensor())
-
-batch_size = 64
-
-train_dataloader = DataLoader(training_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
+def get_params():
+    params = {
+        'features': 256,
+        'lr': 0.001,
+        'momentum': 0,
+    }
+    optimized_params = nni.get_next_parameter()
+    params.update(optimized_params)
+    return params
 
 
+def get_data():
+    training_data = datasets.FashionMNIST(root="data", train=True, download=True, transform=ToTensor())
+    test_data = datasets.FashionMNIST(root="data", train=False, download=True, transform=ToTensor())
+    return training_data, test_data
 
-device = "cpu"
+
+def get_dataloaders(training_data, test_data, batch_size=64):
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+    return train_dataloader, test_dataloader
+
+
+def get_device():
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 class NeuralNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, features):
         super(NeuralNetwork, self).__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, params['features']),
+            nn.Linear(28*28, features),
             nn.ReLU(),
-            nn.Linear(params['features'], params['features']),
+            nn.Linear(features, features),
             nn.ReLU(),
-            nn.Linear(params['features'], 10)
+            nn.Linear(features, 10)
         )
 
     def forward(self, x):
@@ -46,23 +50,18 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
-model = NeuralNetwork().to(device)
-
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=params['lr'], momentum=params['momentum'])
-
-
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
     model.train()
     for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
+        X, y = X.to(device)
         pred = model(X)
         loss = loss_fn(pred, y)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
 
 def test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
@@ -71,7 +70,7 @@ def test(dataloader, model, loss_fn):
     test_loss, correct = 0, 0
     with torch.no_grad():
         for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
+            X, y = X.to(device)
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
@@ -79,9 +78,24 @@ def test(dataloader, model, loss_fn):
     correct /= size
     return correct
 
-epochs = 5
-for t in range(epochs):
-    train(train_dataloader, model, loss_fn, optimizer)
-    accuracy = test(test_dataloader, model, loss_fn)
-    nni.report_intermediate_result(accuracy)
-nni.report_final_result(accuracy)
+
+def main():
+    device = get_device()
+    params = get_params()
+    training_data, test_data = get_data()
+    train_dataloader, test_dataloader = get_dataloaders(training_data, test_data)
+
+    model = NeuralNetwork(params['features']).to(device)
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=params['lr'], momentum=params['momentum'])
+
+    epochs = 5
+    for t in range(epochs):
+        train(train_dataloader, model, loss_fn, optimizer)
+        accuracy = test(test_dataloader, model, loss_fn)
+        nni.report_intermediate_result(accuracy)
+    nni.report_final_result(accuracy)
+
+
+if __name__ == "__main__":
+    main()
