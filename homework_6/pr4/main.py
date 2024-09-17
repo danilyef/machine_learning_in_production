a40cb068c571e5a7a0cd13f638afb9dad3ccbef6
danilyef <daniil.yefimov92@gmail.com>
2024-09-17 20:55:56 +0200

    PR4: Write code to store your model in model management with W&B.

--- homework_6/pr4/main.py
+++ homework_6/pr4/main.py
@@ -3,24 +3,19 @@ import os
 from transformers import DistilBertForSequenceClassification
 
 def main():
-    os.environ["WANDB_API_KEY"] = "0935cbeb7aa9b75ee50eb2235ec860d7ecf8e384"
+    #os.environ["WANDB_API_KEY"] = "define_your_key"
     
-    # Initialize W&B
     wandb.init(project="model_saving", name="distilbert_run")
 
-    # Load or create your DistilBert model
     model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
 
-    # Log the model to W&B
     wandb.watch(model)
 
-    # After training, save the model
     model_artifact = wandb.Artifact("distilbert_model", type="model")
     model.save_pretrained("./distilbert_model")
     model_artifact.add_dir("./distilbert_model")
     wandb.log_artifact(model_artifact)
 
-    # End the W&B run
     wandb.finish()
 
 if __name__ == "__main__":
